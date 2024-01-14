505d54afd3d9c25d05357e05ef06c6e77d032436
Juan Espinoza <jaespin300@yahoo.com>
2024-01-14 16:22:45 -0500

    better styling

--- pages/index.js
+++ pages/index.js
@@ -77,11 +77,6 @@ const Home = () => {
           </div>
           {apiOutput && (
             <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
-                </div>
-              </div>
               <div className="output-content">
                 <p>{apiOutput}</p>
               </div>
